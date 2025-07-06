5403e272744ccfb123bf0ff5436bf48012009060
Shataaa <rafie23ti@mahasiswa.pcr.ac.id>
2025-07-07 01:54:49 +0700

    final part6

--- src/Components/SidebarLogo.jsx
+++ src/Components/SidebarLogo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function SidebarLogo() {
   return (
     <div id="sidebar-logo" className="font-arimo flex flex-col">
-      <img src="src/assets/LogoSneakersX.png" />
+      <img src="/img/SneakersX.png" />
       <span
         id="logo-subtitle"
         className="font-arimo font-semibold text-gray-400 ml-8"
